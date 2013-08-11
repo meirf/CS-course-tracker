aaa7185684f4f3f25be809c8606d41180e78aea8
Meir <meirfischer@gmail.com>
2013-08-11 13:00:33 -0700

    clean up code remove unused imports

--- shab_sched.py
+++ shab_sched.py
@@ -2,22 +2,10 @@
 # -*- coding: utf-8 -*-
 import cgi
 import urllib
-import urllib2
 import os
 import jinja2
 import webapp2
 
-from google.appengine.api import urlfetch
-from random import randint
-from mail_tool import Mailer
-from google.appengine.api import users, urlfetch
-from google.appengine.ext import ndb
-from shab_dates import next_weeks_ltd
-from shab_dates import get_times_quarters
-from shab_dates import get_days_of_week
-from shab_dates import get_deadline_times
-from shab_dates import get_next_fri_sat_dates
-from shab_dates import get_next_weekend_dates
 from course_listing import courses
 from course_listing import adv_courses
 from operator import itemgetter
@@ -28,64 +16,17 @@ JINJA_ENVIRONMENT = \
 
 DEFAULT_MEAL_SCHED_NAME = 'default_meal_sched'
 
-
-# We set a parent key on the 'Meals' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
-
-def meal_sched_key(meal_sched_name=DEFAULT_MEAL_SCHED_NAME):
-    """Constructs a Datastore key for a MealSchedule entity with meal_sched_name."""
-
-    return ndb.Key('MealSchedule', meal_sched_name)
-
-
-class Meal(ndb.Model):
-
-    """Models an individual MealSchedule entry with author, location, and date. """
-
-    author = ndb.UserProperty()
-    location = ndb.StringProperty(indexed=False)
-    date = ndb.DateTimeProperty(auto_now_add=True)
-
-
 class MainPage(webapp2.RequestHandler):
 
     def get(self):       
 
         meal_sched_name = self.request.get('meal_sched_name',
                 DEFAULT_MEAL_SCHED_NAME)
-        meals_query = \
-            Meal.query(ancestor=meal_sched_key(meal_sched_name)).order(-Meal.date)
-        meals = meals_query.fetch(3)
-
-        nickname = ''
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-            nickname = users.get_current_user().nickname()
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        (fri, sat) = get_next_fri_sat_dates()
-        upcoming_shab_dates = get_next_weekend_dates(fri, sat, 4)
-        times = get_times_quarters()
-
-        deadline_days = get_days_of_week()
-        deadline_times = get_deadline_times()
 
         template_values = {
             'courses' : courses,
             'adv_courses': sorted(list(adv_courses), key=itemgetter(1, 2)),
-            'deadline_times': deadline_times,
-            'deadline_days': deadline_days,
-            'nickname': nickname,
-            'times': times,
-            'dates': upcoming_shab_dates,
-            'meals': meals,
             'meal_sched_name': urllib.quote_plus(meal_sched_name),
-            'url': url,
-            'url_linktext': url_linktext,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -96,20 +37,8 @@ class MealSchedule(webapp2.RequestHandler):
 
     def post(self):
 
-        # We set the same parent key on the 'Meal' to ensure each Meal
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-
         meal_sched_name = self.request.get('meal_sched_name',
                 DEFAULT_MEAL_SCHED_NAME)
-        meal = Meal(parent=meal_sched_key(meal_sched_name))
-
-        if users.get_current_user():
-            meal.author = users.get_current_user()
-
-        meal.location = self.request.get('location')
-        meal.put()
 
         query_params = {'meal_sched_name': meal_sched_name}
         self.redirect('/?' + urllib.urlencode(query_params))
@@ -120,22 +49,6 @@ class Verify2Google(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('google-site-verification: google17bd46c295eec9f7.html')
 
-class Courses(webapp2.RequestHandler):
-
-    def get(self):
-        url = "http://www.google.com/"
-        try:
-          result = urllib2.urlopen(url)
-          doSomethingWithResult(result)
-        except urllib2.URLError, e:
-          handleError(e)
-        '''
-        url = "http://shobbus.appspot.com/test"
-        result = urlfetch.fetch(url)
-        if result.status_code == 200:
-            return webapp2.Response('Hello, world!')        
-        '''
-
 class Test(webapp2.RequestHandler):
 
     def get(self):
@@ -145,6 +58,5 @@ application = webapp2.WSGIApplication(
     [('/', MainPage), 
      ('/sign', MealSchedule),
      ('/google17bd46c295eec9f7.html', Verify2Google),
-     ('/courses', Courses),
      ('/test', Test)
      ], debug=True)
