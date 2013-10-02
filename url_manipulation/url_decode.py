78aa76c4bbc154454a06c0350f2f4aecd5565406
Meir <meirfischer@gmail.com>
2013-10-01 23:02:46 -0400

    added comments to response parser

--- url_manipulation/url_decode.py
+++ url_manipulation/url_decode.py
@@ -1,8 +1,13 @@
 import re
 import urlparse
 
+""" Url parsing utilities """
+
 
 def get_url_param_mappings(response):
+    """
+    Parses url response for parameter pairs
+    """
     match = re.search('/(.*\?)(\S*)', response)
     if match:
         return dict(urlparse.parse_qsl(match.groups()[1]))
